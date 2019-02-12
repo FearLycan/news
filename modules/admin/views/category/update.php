7413b37453458d9ed2e3bf2d8f1036f8fb19876e
DamianBronczyk <damian.bronczyk@gmail.com>
2019-02-12 16:17:01 +0100

    update

--- modules/admin/views/category/update.php
+++ modules/admin/views/category/update.php
@@ -11,22 +11,20 @@ $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="category-update">
-
-    <div class="category-create">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title"><i class="fa fa-tag" aria-hidden="true"></i>
-                            Update Category: <?= $model->name ?>
-                        </h3>
-                    </div>
-                    <div class="panel-body">
-                        <?= $this->render('_form', [
-                            'model' => $model,
-                        ]) ?>
-                    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><i class="fa fa-tag" aria-hidden="true"></i>
+                        Update Category: <?= $model->name ?>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <?= $this->render('_form', [
+                        'model' => $model,
+                    ]) ?>
                 </div>
             </div>
         </div>
     </div>
+</div>
