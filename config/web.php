10db4e76c9da544dd4f6b6d3db43ed08de2abcfc
DamianBronczyk <damian.bronczyk@gmail.com>
2019-02-12 16:16:08 +0100

    update config

--- config/web.php
+++ config/web.php
@@ -12,51 +12,7 @@ $config = [
         '@npm' => '@vendor/npm-asset',
     ],
     'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'KP9dvJU_-IHKFRIZgckDsnYBE-HzVpiR',
-        ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'class' => 'app\components\WebUser',
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-            'loginUrl' => ['auth/login'],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
         'db' => $db,
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-                '<alias:admin>' => 'admin/default/index',
-            ],
-        ],
-    ],
-    'modules' => [
-        'admin' => [
-            'class' => 'app\modules\admin\Admin',
-        ],
     ],
     'params' => $params,
 ];
